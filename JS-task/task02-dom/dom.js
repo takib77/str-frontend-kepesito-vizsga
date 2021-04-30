7ad8acb964a3c41b7739c8881b7e06c67b86753b
Leo <takib77@gmail.com>
2021-04-30 10:58:49 +0200

    dom kész

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,19 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    const links = document.querySelectorAll('#link-list a');
+    links.forEach(link => {
+        if (link.innerHTML.includes('outer-link')) {
+            link.setAttribute('target', '_blank');
+            link.innerHTML = `<strong>${link.innerHTML}</strong>`
+        }
+    });
 
+    const nav = document.querySelector('#link-list');
+    nav.style.display = 'flex';
+    nav.style.flexDirection = 'column';
+    nav.style.width = '30%';
+    nav.style.margin = '0px auto';
+    nav.style.border = '1px solid blue';
+    nav.style.padding = '16px';
+}
 
 export { changeOuterLinks };
